de9ab8c26ba5413ebc0951384b07490affd94986
carlos.loaizaact <carlos.loaizaact@wom.com>
2024-04-20 22:10:33 -0500

    .

--- ROM/settings.py
+++ ROM/settings.py
@@ -92,14 +92,22 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 20000
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': ENV.db(),
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     # 'default': ENV.db(),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+import dj_database_url
+from decouple import config
+
+DATABASES = {
+   'default': dj_database_url.config(
+       default=config('DATABASE_URL')
+   )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
