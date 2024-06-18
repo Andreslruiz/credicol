c8aae20826cc4a4b625cf6c07e918e595c8b27fd
carlos.loaizaact <carlos.loaizaact@wom.com>
2024-06-17 22:45:33 -0500

    dias mora

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,10 @@
+from datetime import datetime, date
 from unidecode import unidecode
 
 from rest_framework import serializers
 
 from .models import ClienteProfile
+from transacciones.models import Transaccion
 
 
 class ClientesListSerializer(serializers.ModelSerializer):
@@ -10,6 +12,7 @@ class ClientesListSerializer(serializers.ModelSerializer):
     telefono = serializers.SerializerMethodField()
     nombres = serializers.SerializerMethodField()
     deuda = serializers.SerializerMethodField()
+    dias_mora = serializers.SerializerMethodField()
 
     class Meta:
         model = ClienteProfile
@@ -19,7 +22,8 @@ class ClientesListSerializer(serializers.ModelSerializer):
             'email',
             'telefono',
             'deuda',
-            'activo'
+            'activo',
+            'dias_mora'
         )
 
     def get_telefono(self, obj):
@@ -37,3 +41,28 @@ class ClientesListSerializer(serializers.ModelSerializer):
     def get_deuda(self, obj):
         deuda = obj.deuda
         return deuda
+
+    def get_dias_mora(self, obj):
+        fecha_actual = date.today()
+        deuda = obj.deuda
+        transacciones = Transaccion.objects.filter(cliente=obj)
+        deuda_clean = ''.join(c for c in deuda if c.isdigit())
+
+        if int(deuda_clean) > 0 and transacciones:
+            tr = Transaccion.objects.filter(cliente=obj).order_by('-fecha_transaccion').first()
+            ultimo_pago = tr.fecha_transaccion
+
+            if isinstance(ultimo_pago, datetime):
+                ultimo_pago = ultimo_pago.date()
+
+            diferencia = fecha_actual - ultimo_pago
+
+            dias_pasados = diferencia.days
+
+        else:
+            dias_pasados = 0
+
+        if dias_pasados > 0:
+            return dias_pasados
+
+        return ''
