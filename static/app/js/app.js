c4bc63ee8de244177b07f4891ecba5c7aa8a22a6
carlos.loaizaact <carlos.loaizaact@wom.com>
2024-06-20 00:19:37 -0500

    -

---
+++ static/app/js/app.js
@@ -0,0 +1,52 @@
+
+// let swLocation = "sw.js";
+
+// console.log('Init serviceWorker')
+// if (navigator.serviceWorker) {
+//   var domain = window.location.hostname;
+//   console.log(domain);
+//   if (window.location.href.includes(domain)) swLocation = "/static/app/js/sw.js";
+//   navigator.serviceWorker.register(swLocation);
+// }
+
+const checkPermission = () => {
+  if(!('serviceWorker') in navigator){
+    throw new Error('ServiceWorker no soportado')
+  }
+
+  if(!('Notification') in window){
+    throw new Error('Notificaciones no soportadas')
+  }
+}
+
+const registerSW = async () => {
+  const registration = await navigator.serviceWorker.register('/static/app/js/sw.js');
+  return registration;
+}
+
+const requestNotificationPermission = async () => {
+  const permission = await Notification.requestPermission();
+  if (permission !== 'granted') {
+    console.warn('El usuario ha denegado el permiso para notificaciones');
+  }
+}
+
+const main = async () => {
+  checkPermission();
+  requestNotificationPermission();
+  // await requestNotificationPermission();
+  // await registerSW();
+
+  const lastNotificationDate = localStorage.getItem('lastNotificationDate');
+  const today = new Date().toDateString();
+
+  if (lastNotificationDate !== today) {
+    const reg = await registerSW();
+    reg.showNotification('Hola!', {
+      body: '¡Bienvenido a CrediCol App!'
+    });
+    localStorage.setItem('lastNotificationDate', today);
+  }
+}
+
+main();
