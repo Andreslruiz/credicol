c1d84f2985725829fe2e8ecef988300f78150dd4
carlos.loaizaact <carlos.loaizaact@wom.com>
2024-07-25 16:07:11 -0500

    profile image

--- common/services.py
+++ common/services.py
@@ -15,25 +15,25 @@ def send_payment_notify(user, to, username, balance):
 
     name = username.title()
     total = balance
-    company = company.name.title()
+    company_name = company.name.title()
 
     base_url = "https://api.textmebot.com/send.php"
     message_template = (
-        "🚨 _*{company}*_\n\n"
+        "🚨 _*{company_name}*_\n\n"
         "¡Hola {name}!\n"
         "¡Pago Realizado! ✅ Queremos informarte amablemente que el valor restante de "
         "tus compras fiadas es de:\n\n"
         "*RESTANTE POR PAGAR:*\n"
         "*${total}*\n\n"
         "Un gusto atenderte.\n\n"
-        "Saludos cordiales, {company}\n\n"
+        "Saludos cordiales, {company_name}\n\n"
         "_CrediCol Colombia 2024_"
     )
 
-    message = message_template.format(name=name, total=total, company=company)
+    message = message_template.format(name=name, total=total, company_name=company_name)
     encoded_message = urllib.parse.quote(message)
 
-    full_url = f"{base_url}?recipient={urllib.parse.quote(recipient)}&apikey={apikey}&text={encoded_message}"
+    full_url = f"{base_url}?recipient={urllib.parse.quote(recipient)}&apikey={apikey}&text={encoded_message}&file={company.company_image_url}"
     response = requests.get(full_url)
     return response.status_code == 200
 
@@ -98,7 +98,7 @@ def remember_payment_notify(user, to, username, balance, days_overdue):
     message = message_template.format(name=name, total=total, contact=contact, days_overdue=days_overdue, company_name=company_name)
     encoded_message = urllib.parse.quote(message)
 
-    full_url = f"{base_url}?recipient={urllib.parse.quote(recipient)}&apikey={apikey}&text={encoded_message}"
+    full_url = f"{base_url}?recipient={urllib.parse.quote(recipient)}&apikey={apikey}&text={encoded_message}&file={company.company_image_url}"
     response = requests.get(full_url)
     return response.status_code == 200
 
